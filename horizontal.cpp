fc6877484868d8b982e3deb4bdf5054649e26d24
Raker2000 <85421237+Raker2000@users.noreply.github.com>
2023-10-21 18:29:56 -0300

    Comp. horizontal añadida, falta ruta horizontal

--- horizontal.cpp
+++ horizontal.cpp
@@ -7,6 +7,10 @@ Horizontal::Horizontal(int x, int y, Tablero* t) : Estacion(x, y, t)
 
 bool Horizontal::comprobaciones()
 {
-    //if(this->tablero->getEnPos(this->getY(),this->getX())==5)//esto tiene que ser 6, no 5
-    return true;
+    if(this->tablero->getEnPos(this->getX(),this->getY()-1)==6 || this->tablero->getEnPos(this->getX(),this->getY()+1)==6)
+    {
+        std::cout<<"Estacion "<<this->getTipo()<<" conectada con exito"<<std::endl;
+        return true;
+    }
+    return false;
 }
