7664d43b12683a4b18f60815da73c9ab7fca745e
aleledesma <aleledesma1212@gmail.com>
2023-11-11 18:17:08 -0300

    solcucion conexion estaciones comunes

--- comun.cpp
+++ comun.cpp
@@ -9,14 +9,15 @@ bool Comun::comprobaciones(int filaDeColocacion, int columnaDeColocacion)
 {
     //si se usa como vertical:
     if(filaDeColocacion != this->getX()) {
-        return (tablero->getEnPos(this->getX(), this->getY() - 1) == 0) && (tablero->getEnPos(this->getX(), this->getY() + 1) == 0);
+        int izq = this->tablero->getEnPos(this->getX(), this->getY() - 1);
+        int der = this->tablero->getEnPos(this->getX(), this->getY() + 1);
+        return ((izq == 0 || izq == -1) && (der == 0 || der == -1));
     }
     //si se usa como horizontal:
     else if(columnaDeColocacion != this->getY()) {
-        return (tablero->getEnPos(this->getX() - 1, this->getY()) == 0) && (tablero->getEnPos(this->getX() + 1, this->getY()) == 0);
+        int arr = tablero->getEnPos(this->getX() - 1, this->getY());
+        int abj = tablero->getEnPos(this->getX() + 1, this->getY());
+        return ((arr == 0 || arr == -1) && (abj == 0 || abj == -1));
     }
-
-    //falta abarcar el caso en que la estacion este colocada a un borde.
-
 }
 
